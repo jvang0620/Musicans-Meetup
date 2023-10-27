1b8f4dd35559a69b69d736bb36335c9067979dd3
Jai_Vang <jaivang0387@gmail.com>
2023-10-27 11:13:01 -0400

    deleting comments

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,10 +2,7 @@
 * This controller module consists of the call back functions from the route module
 **********************************************************************************/
 
-
-
-//import module event
-//*Remember, whenever you import a module you specify yourself you need to include the path
+//require: import module event
 const model = require('../models/event');
 
 
@@ -13,12 +10,8 @@ const model = require('../models/event');
 * Anonymous function set to exports.index. Can be call later.
 ***********************************************************/
 exports.index = (req, res) => {
-    // res.send('send all events'); //uncomment for testing
-    // res.send(model.find()); //uncomment for testing
-
     let events = model.find();
     let getAllCategories = model.getAllCategories();
-    
     res.render('./event/indexEvents.ejs', {events, getAllCategories});
 };
 
@@ -26,7 +19,6 @@ exports.index = (req, res) => {
 * Used to send new form
 ***********************/
 exports.new = (req, res) => {
-    //res.send('send the new form');
     res.render('./event/newEventForm.ejs');
 };
 
@@ -35,19 +27,9 @@ exports.new = (req, res) => {
 * Creation of the new events. Create new object in array
 ********************************************************/
 exports.create = (req, res) => {
-    // res.send('Created a new event'); //uncomment for testing
-    // console.log(req.body); //uncomment for testing
 
     let event = req.body;
-
-    //Uncomment for testing
-    // console.log(req.file);
-    // console.log("File name:\n" + req.file.originalname) 
     event.image = '/images/img-upload/' + req.file.filename;
-
-    //Uncomment for testing
-    // console.log(event);
-
     model.save(event);
     res.redirect('/events');
 };
@@ -59,15 +41,12 @@ exports.create = (req, res) => {
 exports.show = (req, res, next) => {
     let id = req.params.id;
     let event = model.findById(id);
-    // res.send('send event with id ' + req.params.id); //uncomment for testing
-    // res.send(event); //For testing //uncomment for testing
-    // res.render('./event/show', {event}); //uncomment for testing
 
     // if event is not undefined
     if(event) {
         res.render('./event/showEvent.ejs', {event});
-    } else {
-        //res.status(404).send('Cannot find event with id ' + id); //uncomment for testing
+    } 
+    else {
         let err = new Error('Cannot find a event with id ' + id);
         err.status = 404;
         next(err);
@@ -93,7 +72,6 @@ exports.edit = (req, res, next) => {
         err.status = 404;
         next(err);
     }
-    // res.send('send event edit form');
 };
 
 
@@ -101,7 +79,6 @@ exports.edit = (req, res, next) => {
 * For updating an event
 ***********************/
 exports.update = (req, res, next) => {
-    // res.send('update event with id ' + req.params.id);
     let event = req.body;
     let id = req.params.id;
 
@@ -115,7 +92,6 @@ exports.update = (req, res, next) => {
         res.redirect('/events/' + id);
     }
     else {
-        // res.status(404).send('Cannot find event with id ' + id); //uncomment for testing
         let err = new Error('Cannot find a event with id ' + id);
         err.status = 404;
         next(err);
@@ -127,7 +103,6 @@ exports.update = (req, res, next) => {
 * For deleting an event
 ***********************/
 exports.delete = (req, res, next) => {
-    // res.send('delete event with id ' + req.params.id);
     let id = req.params.id;
 
     // //if true
@@ -135,7 +110,6 @@ exports.delete = (req, res, next) => {
         res.redirect('/events');
     }
     else {
-        // res.status(404).send('Cannot find sotry with id ' + id);
         let err = new Error('Cannot find a event with id ' + id);
         err.status = 404;
         next(err);
