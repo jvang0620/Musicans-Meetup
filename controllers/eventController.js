6015cefd6a906e72f3127a38d66ac2d26042422e
Jai_Vang <jaivang0387@gmail.com>
2023-10-27 12:19:09 -0400

    refactoring the application for showing event details

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -71,17 +71,29 @@ exports.create = (req, res, next) => {
 **************************/
 exports.show = (req, res, next) => {
     let id = req.params.id;
-    let event = model.findById(id);
 
-    // if event is not undefined
-    if(event) {
-        res.render('./event/showEvent.ejs', {event});
-    } 
-    else {
-        let err = new Error('Cannot find a event with id ' + id);
-        err.status = 404;
-        next(err);
+    //an objectID is a 24-bit Hex String
+    //id has to follow this pattern. It can only contain 0-9, lowercase/uppercase a through f, and has to be 24 digits. 
+    if(!id.match(/^[0-9a-fA-F]{24}$/)) { 
+        let err = new Error('Invalid event id');
+        err.status = 400;
+
+        return next(err);
     }
+
+    model.findById(id)
+    .then(event => {
+        // if event is not undefined
+        if(event) {
+            res.render('./event/showEvent.ejs', {event});
+        } 
+        else {
+            let err = new Error('Cannot find a event with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err => next(err));
 };
 
 
