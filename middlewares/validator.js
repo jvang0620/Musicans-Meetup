77fd4e8e3639b513fa28b4d14b7f04983d4d4b8e
Jai_Vang <jaivang0387@gmail.com>
2023-12-06 12:49:17 -0500

    debuging, adding notEmpty() to some fields, adding and deleting some comments

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,3 +1,8 @@
+/******************************************************************
+* This class is used to enhanced security of the Server-side inputs
+******************************************************************/
+
+
 // In terminal: npm install express-validator
 
 const {body, validationResult} = require('express-validator');
@@ -5,7 +10,9 @@ const Event = require('../models/event');
 const Rsvp = require('../models/rsvp');
 
 
-//middleware function that validate ID
+/*************************************
+* middleware function that validate ID 
+*************************************/
 exports.validateId = (req, res, next) => {
     const id = req.params.id;
 
@@ -21,23 +28,29 @@ exports.validateId = (req, res, next) => {
 }
 
 
-//middleware function that validate sign up inputs
+/*************************************************
+* middleware function that validate sign up inputs
+**************************************************/
 exports.validateSignup = [
   body('firstName', 'First name field is required').notEmpty().trim().escape(),
   body('lastName', 'Last name field is required').notEmpty().trim().escape(),
-  body('email', 'Email field is required').isEmail().trim().escape().normalizeEmail(),
-  body('password', 'Password must be at least 8 characters and at most 64 characters').isLength({min: 8, max: 64})];
+  body('email', 'Email field is required').notEmpty().isEmail().trim().escape().normalizeEmail(),
+  body('password', 'Password must be at least 8 characters and at most 64 characters').notEmpty().trim().isLength({min: 8, max: 64})];
 
 
-//middleware function that validate Log in inputs
+/*************************************************
+* middleware function that validate Log in inputs
+**************************************************/
 exports.validateLogIn = [
-  body('email', 'Email is not valid').notEmpty().isEmail().trim().escape().normalizeEmail(),
-  body('password', 'Password must be at least 8 characters and at most 64 characters').notEmpty().isLength({min: 8, max: 64})];
+  body('email', 'Email is not valid. Should be a format like this (abcdef@gmail.com)').notEmpty().isEmail().trim().escape().normalizeEmail(),
+  body('password', 'Password must be at least 8 characters and at most 64 characters').notEmpty().trim().isLength({min: 8, max: 64})];
 
 
-//middleware function that validate creating/editing event
+/*********************************************************
+* middleware function that validate creating/editing event
+*********************************************************/
 exports.validateEvent = [
-  body('category', 'Category is required').notEmpty()
+  body('category', 'Category is required')
 
   //add custom validation function
   .custom((value, { req }) => {
@@ -51,19 +64,12 @@ exports.validateEvent = [
     else {
       return true;
     }
-  }).trim().escape(),
-
-  //Orgingal code (Will leave this comment in case I need to revert back)
-  // body('category', 'Category is required').notEmpty().isIn(Event.schema.path('category').enumValues).trim().escape(),
-
-  //2nd change (Will leave this comment in case I want to make additional changes)
-  // body('category').if((value, { req }) => req.body.category !== undefined).notEmpty().isIn(Event.schema.path('category').enumValues).withMessage('Category must be one of the following: Music Talk, Meet Up, Rehearsal, Song Writing, Try Outs, Others').trim().escape(),
-
+  }).notEmpty().trim().escape(),
 
   body('title', 'Title is required').notEmpty().trim().escape(),
   body('details', 'Details must be at least 10 characters').notEmpty().isLength({min: 10}).trim().escape(),
   body('startDateTime', 'Error! Start time must be after the current time!').notEmpty().isISO8601().isAfter().trim().escape(),
-  body('endDateTime', 'Error!!! End time must be after start time!').notEmpty().isISO8601()
+  body('endDateTime', 'Error! End time must be after start time!').notEmpty().isISO8601()
 
     //add custom validation function
     .custom((value, { req }) => {
@@ -73,46 +79,27 @@ exports.validateEvent = [
       // Parses the startDateTime string from the request body into a JavaScript Date object
       start = Date.parse(req.body.startDateTime)  
 
-      //if the 'startDateTime' is greater than or equal to the 'endDateTime' (EX: [StartDateTime (12/6/23 1pm) is greater than EndDateTime (12/1/23 1pm)]   or    [StartDateTime (12/1/23 1pm) is equal to EndDateTime (12/1/23 1pm)].
+      //if the 'startDateTime' is greater than or equal to the 'endDateTime' 
+      //(EX: [StartDateTime (12/6/23 1pm) is greater than EndDateTime (12/1/23 1pm)]   
+      //or    [StartDateTime (12/1/23 1pm) is equal to EndDateTime (12/1/23 1pm)].
       if (start >= end) {
         throw new Error("Error! End time must be after start time!");
       }
-      //the 'startDateTime' is less than the 'endDateTime' (EX: StartDateTime (12/6/23 1pm) is greater than EndDateTime (12/6/23 2pm)
+      //the 'startDateTime' is less than the 'endDateTime' 
+      //(EX: StartDateTime (12/6/23 1pm) is greater than EndDateTime (12/6/23 2pm)
       else {
         return true;
       }
     })
     .trim().escape(),
   body('location', 'Location is required').notEmpty().trim().escape(),
-  body('image', 'Image is required')
-    .custom((value, { req }) => {
 
-      //check if the request method is POST
-      if(req.method === 'POST') {
-
-        // check if there are no files in the request
-        if(!req.files) {
-          // indicates validation failure, no image file provided
-          return false;
-        }
-        // Check the file type and see if image file type is not one of these
-        if(req.files.image.mimetype !== 'image/png' 
-        && req.files.image.mimetype !== 'image/jpg' 
-        && req.files.image.mimetype !== 'image/jpeg' 
-        && req.files.image.mimetype !== 'image/gif') {
-          // indicates validation failure, unsupported file type
-          return false;
-        }
-      }
-      else {
-        // indicates validation success
-        return true;
-      }
-    })
 ];
 
 
-//middleware function that validate the RSVP status field to only be 'YES', 'NO', or 'MAYBE'
+/*******************************************************************************************
+* middleware function that validate the RSVP status field to only be 'YES', 'NO', or 'MAYBE'
+********************************************************************************************/
 exports.validateRsvp = [
   body('status')
     .notEmpty().withMessage('RSVP cannot be empty')
@@ -133,14 +120,9 @@ exports.validateRsvp = [
 ];
 
 
-//Orginal exports.validateRsvp (Leave comment in case I want to revert back)
-// exports.validateRsvp = [
-//   body('status', 'RSVP cannot be empty').notEmpty().trim().escape(),
-//   body('status', `RSVP can only be one of the following: ${Rsvp.schema.path('status').enumValues.join(', ')}`).trim().escape().isIn(Rsvp.schema.path('status').enumValues)
-// ];
-
-
-//middleware function that validate the result
+/*********************************************
+* middleware function that validate the result
+*********************************************/
 exports.validateResult = (req, res, next) => {
   //validationResult takes 'req' object
   let errors = validationResult(req);
